6a9a477193490dea70686db31a0a4a80d86fa9ae
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2022-12-27 09:35:05 -0300

    little refactor

--- pkg/binary_heap/heap.go
+++ pkg/binary_heap/heap.go
@@ -4,12 +4,14 @@ package binary_heap
  * A binary heap must satisfy two properties:
  *
  *	1. The heap property:
- *	- In a max-heap, the key of the parent must be greater than the keys of its children. The root of the heap holds the greatest value.
- *	- In a min-heap, the key of the parent must be smaller than the keys of its children. The root of the heap holds the smallest value.
+ *	  - In a max-heap, the key of the parent must be greater than the keys of its children.
+ *      The root of the heap holds the greatest value.
+ *	  - In a min-heap, the key of the parent must be smaller than the keys of its children.
+ *      The root of the heap holds the smallest value.
  *
  *	2. The shape property:
- *	- A binary heap must be a complete binary tree, e.g., at every level,
- *	except possibly the last, the tree is completely filled, and all nodes in the last level are as far left as possible.
+ *	  - A binary heap must be a complete binary tree, e.g., at every level,
+ *	    except possibly the last, the tree is completely filled, and all nodes in the last level are as far left as possible.
  *
  */
 
@@ -17,18 +19,17 @@ package binary_heap
 // TODO: this has been implemented as a max-heap, but you should implement it a min-heap too.
 
 /*
- *
  * This method is suboptimal. It is here for my own educational purposes.
  * It even has a fancy name: Williams' method, after the guy who came up with binary heaps in the 60s.
  *
  * It's based on this idea:
- *   1. Start with a empty heap.
+ *   1. Start with an empty heap.
  *   2. Push() each element from the input list.
  *
  * The reason it is suboptimal is because each Push() operation runs on *O(log n)*,
  * and we do it for each element on the input list, so this method runs on *O(n log n)*.
  *
- * */
+ */
 func SuboptimalToHeap(list []int) []int {
 	heap := []int{}
 	for _, e := range list {
@@ -64,11 +65,19 @@ func Push(heap []int, e int) []int {
 	heap = append(heap, e)
 
 	// Move the new element up, until heap property is satisfied.
-	new := up(heap, len(heap)-1)
-	return new
+	return up(heap, len(heap)-1)
 }
 
-// TODO: this one needs a good comment.
+/*
+ * The Pop() operation has a similar logic to the Push() operation, but it's kind of inversed:
+ *   1. Swap the current root with the last element in the tree.
+ *   2. Move the new root (previously last element) down until the heap property is satisfied.
+ *      We do that by comparing the parent with its children: if the parent already satisfies
+ *      the heap property (for min-heap, it's smaller and its children; for max-heap, it's bigger),
+ *      there's nothing to do, just stop. If it still doesn't, we swap the parent with a child
+ *      (the biggest child if max-heap, and the smallest child if min-heap).
+ *      We do this until the condition is no longer true.
+ */
 func Pop(heap []int) ([]int, int) {
 	if len(heap) == 0 {
 		return []int{}, -1
@@ -112,46 +121,41 @@ func up(heap []int, childIndex int) []int {
 func down(heap []int) []int {
 	parentIndex := 0
 
-	// iterate until the current parent has no children
-	for parentIndex*2+1 < len(heap) {
+	for {
 		// The children of a parent at index i are always at [2i+1, 2i+2]
-		left := heap[parentIndex*2+1]
+		leftChildIndex := parentIndex*2 + 1
+		rightChildIndex := parentIndex*2 + 2
 
-		// TODO: there must be a better to write this
-		right := 0
-		if parentIndex*2+2 < len(heap) {
-			right = heap[parentIndex*2+2]
+		// If the current node has no children, we stop
+		if leftChildIndex > len(heap)-1 {
+			break
 		}
 
-		if left > right {
+		var childIndex int
 
-			// if parent is already greater than max child, stop
-			if heap[parentIndex] > left {
-				break
-			}
-
-			// swap parent with left child
-			x := heap[parentIndex]
-			heap[parentIndex] = left
-			heap[parentIndex*2+1] = x
-
-			// and move down the tree
-			parentIndex = parentIndex*2 + 1
+		// If the current node has no right child, left child is the biggest one.
+		if rightChildIndex > len(heap)-1 {
+			childIndex = leftChildIndex
+		} else if heap[rightChildIndex] > heap[leftChildIndex] {
+			// If the current node has a right child, and it is bigger than the left one, we use it.
+			childIndex = rightChildIndex
 		} else {
-
-			// if parent is already greater than max child, stop
-			if heap[parentIndex] > right {
-				break
-			}
-
-			// swap parent with right child
-			x := heap[parentIndex]
-			heap[parentIndex] = right
-			heap[parentIndex*2+2] = x
-
-			// and move down the tree
-			parentIndex = parentIndex*2 + 2
+			// If the current node has a right child, but it is not bigger than the left one, we use the left one.
+			childIndex = leftChildIndex
 		}
+
+		// If parent is already greater than the biggest child, stop.
+		if heap[parentIndex] > heap[childIndex] {
+			break
+		}
+
+		// Otherwise, we swap the parent with its child.
+		x := heap[parentIndex]
+		heap[parentIndex] = heap[childIndex]
+		heap[childIndex] = x
+
+		// And move down the tree.
+		parentIndex = childIndex
 	}
 
 	return heap
